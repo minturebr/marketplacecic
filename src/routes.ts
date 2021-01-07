51cc8e31b299fbf9c1c12cb458d7dc42726da162
Julio Eduardo <julio.eduardo@protonmail.com>
2021-01-06 23:53:11 -0300

    feat: add route for book upload

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 import multer from 'multer'
-import { multerConfigCatalog } from './config/multer'
+import { multerConfigCatalog, multerConfigBook } from './config/multer'
 
 import SellerController from './controllers/SellerController'
 import CatalogController from './controllers/CatalogController'
+import BookController from './controllers/BookController'
 
 const routes = Router()
 
 routes.get('/sellers', SellerController.index)
 routes.post('/sellers', SellerController.store)
 routes.post('/catalog', multer(multerConfigCatalog).single('file'), CatalogController.store)
+routes.post('/book', multer(multerConfigBook).single('file'), BookController.store)
 
 export default routes
